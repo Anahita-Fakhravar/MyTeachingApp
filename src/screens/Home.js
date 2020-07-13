c88c36460f2682acfcfebb289125dd382406923a
ASUS <anahita_fakhravar@yahoo.com>
2020-07-13 09:56:42 +0430

    Add custom top bar to home page

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View,Text} from 'react-native';
+import CustomTopBar from '../components/CustomTopBar';
 
 const Home = (Props)=>{
 
     return (
         <View>
+            <CustomTopBar/>
             <Text>{Props.userName}</Text>
         </View>
     )
