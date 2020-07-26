9084a1853bc23e46a5970e47d829332271096b48
ASUS <anahita_fakhravar@yahoo.com>
2020-07-27 00:12:05 +0430

    Teach FlexBox - Part 2

--- src/screens/StaggeredView.js
+++ src/screens/StaggeredView.js
@@ -1,18 +1,39 @@
+//StaggeredView
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import {responsiveHeight, responsiveWidth} from 'react-native-responsive-dimensions';
 
 const StaggeredView = () => {
 
 
     return (
-        <View style={{backgroundColor:'yellow',
+
+        <View style={{
+            flexDirection:'row',
+            flexWrap:'wrap',
+            backgroundColor:'red',
             flex:1,
-            flexDirection:'row-reverse'
+            justifyContent:'center',
+            alignContent:'center'
         }}>
-            <Text style={{backgroundColor:'green',flex:1}}>100</Text>
-            <Text style={{backgroundColor:'blue',flex:1}}>100</Text>
-            <Text style={{backgroundColor:'red',flex:2}}>100</Text>
+
+            <Text style={styles.txt}>100</Text>
+            <Text style={styles.txt}>200</Text>
+            <Text style={styles.txt}>300</Text>
+
         </View>
     );
 };
 export default StaggeredView;
+const styles = StyleSheet.create({
+    txt: {
+        backgroundColor: 'black',
+        color: 'white',
+        width: responsiveWidth(20),
+        height: responsiveHeight(12),
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        borderRadius: responsiveWidth(2),
+        margin: responsiveWidth(2),
+    },
+});
