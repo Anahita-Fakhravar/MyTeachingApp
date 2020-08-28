eddb27579df14674bbf521edf95e98101de8fbe1
ASUS <anahita_fakhravar@yahoo.com>
2020-08-28 23:49:49 +0430

    Define images in Constant.js

--- src/Constant.js
+++ src/Constant.js
@@ -5,3 +5,8 @@ export const loginImg = require('./assets/images/food.png');
 export const menu = require('./assets/images/menuIcon.png');
 export const close = require('./assets/images/close.png');
 export const sideMenuImg = require('./assets/images/sideMenuImg.png');
+
+export const deliveryImg = require('./assets/images/deliveryImg.png');
+export const healthyFoodImg = require('./assets/images/healthyFoodImg.png');
+export const romanticImg = require('./assets/images/romanticImg.png');
+export const deliciousImg = require('./assets/images/deliciousImg.png');
